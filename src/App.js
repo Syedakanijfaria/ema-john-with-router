3d47a9e22ed10acf6443900e961255be88c7edbc
SKFaria <fariafaria737@gmail.com>
2021-10-01 16:27:42 +0600

    problem

--- src/App.js
+++ src/App.js
@@ -1,12 +1,34 @@
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import './App.css';
 import Header from './components/Header/Header';
+import Inventory from './components/Iventory/Inventory.js';
+import NotFound from './components/NotFound/NotFound.js';
+import OrderReview from './components/OrderReview/OrderReview.js';
 import Shop from './components/Shop/Shop';
 
 function App() {
   return (
     <div>
       <Header></Header>
-      <Shop></Shop>
+      <Router>
+        <Switch>
+          <Route exact path="/">
+            <Shop></Shop>
+          </Route>
+          <Route exact path="/shop">
+            <Shop></Shop>
+          </Route>
+          <Route exact path="/orders">
+            <OrderReview></OrderReview>
+          </Route>
+          <Route exact path="/inventory">
+            <Inventory></Inventory>
+          </Route>
+          <Route path="*">
+            <NotFound></NotFound>
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
